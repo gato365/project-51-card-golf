4f6345338525f860088b43ecb423fc16844ae168
gato365 <immanuelwilliams@gato365.com>
2023-12-15 21:25:42 -0800

    Remove unnecessary __len__ method from Deck class

--- golf_card_game.py
+++ golf_card_game.py
@@ -34,8 +34,6 @@ class Deck:
         VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         self.cards = [Card(suit, value) for suit in SUITS for value in VALUES]
         
-    def __len__(self):
-        return len(self.cards)
 
     def shuffle(self):
         random.shuffle(self.cards)
